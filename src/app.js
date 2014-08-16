2040614ef083e3f1e520ed2ac7fdbf16e1e12b30
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-08-17 01:23:48 +0800

    Moves Pointer

--- src/app.js
+++ src/app.js
@@ -10,15 +10,41 @@ $(function(){
     var eyeTracker = new camgaze.EyeTracker(width, height);
     var eyeFilter = new camgaze.EyeFilter();
     var drawer = new camgaze.drawing.ImageDrawer();
+    var movement_x = [];
+    var movement_y = [];
 
     var previous_y = null;
     var previous_x = null;
+    var move = true;
+
+    var recur = setInterval(function(){
+        move = true;
+        if (movement_x.length > 1000) {
+            movement_x = [];
+        }
+
+        if (movement_y.length > 1000) {
+            movement_y = [];
+        }
+        //previous_y = null;
+        //previous_x = null;
+    }, 1000);
+
+    var getAverageX = function(){
+        var sum = 0;
+        var moves = movement_x.slice(Math.max(movement_x.length - 12, 1))
+        moves.forEach(function(elmt){
+            sum += elmt;
+        });
+
+        return sum/12;
+    }
 
     var moveGaze = function(gaze) {
       current_eye = gaze[0];
       eye_data = current_eye.centroid.unfiltered;
 
-      eye_x = eye_data.x;
+      eye_x = eye_data.x + 65;
       eye_y = eye_data.y;
       window_y = $(window).height();
       window_x = $(window).width();
@@ -29,15 +55,35 @@ $(function(){
       current_y = eye_y * y_multiplier;
       current_x = window_x - (eye_x * x_multiplier);
 
-      if ( previous_x == null || (previous_x - 150) <= current_x && (previous_x + 150) >= current_x) {
-        $("#the-eye").css("left", current_x);
-        previous_x = current_x;
+      if (movement_x.length < 12) {
+          movement_x.push(current_x);
       }
 
-      if ( previous_y == null || (previous_y - 150) <= current_y && (previous_y + 150) >= current_y) {
+      if (movement_y.length < 12) {
+          movement_y.push(current_y);
+      }
+
+      //$("#the-eye").animate({
+        //top: current_y,
+        //left: current_x
+      //}, 500);
+      //if ( previous_x == null || (previous_x - 150) <= current_x && (previous_x + 150) >= current_x) {
+      movement_x.push(current_x);
+      aveX = getAverageX();
+
+      //if (((aveX - 100) <= current_x && (aveX + 100) >= current_x) ||
+         //((aveX - 500) >= current_x && (aveX + 500) <= current_x)) {
+        $("#the-eye").css("left", aveX);
+        //movement_x.push(current_x);
+      //}
+        //$("#the-eye").css("left", current_x);
+        //previous_x = current_x;
+      //}
+
+      //if ( previous_y == null || (previous_y - 150) <= current_y && (previous_y + 150) >= current_y) {
         $("#the-eye").css("top", current_y);
         previous_y = current_y;
-      }
+      //}
     }
 
     var frameOp = function (image_data, video) {
